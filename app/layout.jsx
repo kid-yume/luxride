a2553498ada33a9c43d599316a3dadde77b40f04
Agent Yume <eggboyx@icloud.com>
2024-07-02 20:07:25 -0400

    Brand Updates

--- app/layout.jsx
+++ app/layout.jsx
@@ -33,7 +33,11 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={DM_SansFont.variable}>{children}</body>
     </html>
   );
